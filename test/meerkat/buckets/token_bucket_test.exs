2c3420c6fefd1bdb18edae726312697a4f3098ef
James Every <james.every@zuhlke.com>
2016-12-23 13:28:58 +0000

    chore: test bucket refill message

--- test/meerkat/buckets/token_bucket_test.exs
+++ test/meerkat/buckets/token_bucket_test.exs
@@ -4,47 +4,46 @@ defmodule Meerkat.Buckets.TokenBucketTest do
   use ExCheck
   doctest Meerkat.Buckets.TokenBucket
 
-  import Meerkat.Buckets.TokenBucket
+  alias Meerkat.Buckets.TokenBucket, as: TokenBucket
 
   test "Bucket with zero rate limit is empty" do
-    {:ok, pid} = start({0, :per, :second}, [refill: false])
-    assert empty?(pid)
+    {:ok, pid} = TokenBucket.start({0, :per, :second}, [refill: false])
+    assert TokenBucket.empty?(pid)
   end
 
   property :bucket_of_size_n_has_n_tokens do
-    for_all {n} in {non_neg_integer} do
-      {:ok, pid} = start({n, :per, :second}, [refill: false])
-      Enum.map(Range.new(1,n), fn(_) -> refute empty?(pid) end)
-      assert empty?(pid)
+    for_all {n} in {pos_integer} do
+      {:ok, pid} = TokenBucket.start({n, :per, :second}, [refill: false])
+      Enum.map(Range.new(1,n), fn(_) -> refute TokenBucket.empty?(pid) end)
+      assert TokenBucket.empty?(pid)
     end
   end
 
-# TODO how do we send an info message to a process?
-#  property :buckets_get_refilled do
-#    for_all {n} in {pos_integer} do
-#      {:ok, pid} = start({n, :per, :second}, [refill: false])
-#      Enum.map(Range.new(1,n), fn(_) -> refute empty?(pid) end)
-#      assert empty?(pid)
-#      Process.send(pid, :refill)
-#      refute empty?(pid)
-#    end
-#  end
+  property :buckets_get_emptied_and_refilled do
+    for_all {n} in {pos_integer} do
+      {:ok, pid} = TokenBucket.start({n, :per, :second}, [refill: false])
+      Enum.map(Range.new(1,n), fn(_) -> refute TokenBucket.empty?(pid) end)
+      assert TokenBucket.empty?(pid), "Not all tokens drained"
+      :ok = Process.send(pid, :refill, [])
+      assert false == TokenBucket.empty?(pid), "Bucket remains empty after refill"
+    end
+  end
 
   # dec_to_zero
 
   test "Dec zero stays zero" do
-    assert 0 == dec_to_zero(0)
+    assert 0 == TokenBucket.dec_to_zero(0)
   end
 
   property :dec_positive_numbers do
     for_all {n} in {pos_integer} do
-      assert n-1 == dec_to_zero(n)
+      assert n-1 == TokenBucket.dec_to_zero(n)
     end
   end
 
   property :dec_negative_numbers_are_zero do
     for_all {n} in {pos_integer} do
-      assert 0 == dec_to_zero(-n)
+      assert 0 == TokenBucket.dec_to_zero(-n)
     end
   end
 
