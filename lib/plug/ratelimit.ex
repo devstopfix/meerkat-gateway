427987626c949466ef09d415dcef81e5e4becfbd
devstopfix <devstopfix@gmail.com>
2017-02-01 18:49:24 +0000

    Tidy

--- lib/plug/ratelimit.ex
+++ lib/plug/ratelimit.ex
@@ -1,8 +1,8 @@
 defmodule Plug.Ratelimit do
 
-	@moduledoc """
-	A plug that enforces rate limiting (requests per second)
-	"""
+  @moduledoc """
+  A plug that enforces rate limiting (requests per second)
+  """
 
   import Plug.Conn
   #alias Plug.Conn
@@ -10,20 +10,20 @@ defmodule Plug.Ratelimit do
 
   def init(_opts) do
     {:ok, pid} = TokenBucket.start({4, :per, :second}, [refill: false])
-  	{pid}
+    {pid}
   end
 
   def call(conn, {bucket}) do
-  	case TokenBucket.empty?(bucket) do
+    case TokenBucket.empty?(bucket) do
       true  -> too_many_requests(conn)
-  		false -> conn
-  	end
+      false -> conn
+    end
   end
 
   defp too_many_requests(conn) do
-  	conn
-  	|> send_resp(429, "Too Many Requests")
-  	|> halt
+    conn
+    |> send_resp(429, "Too Many Requests")
+    |> halt
   end
 
 end
